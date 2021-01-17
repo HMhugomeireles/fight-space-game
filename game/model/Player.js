6a8e3a9b029ef1a2f86f628121bfbab205388328
Hugo Meireles <hugo.meireles@edirectinsurance.com>
2021-01-17 04:02:27 +0000

    create player model

---
+++ game/model/Player.js
@@ -0,0 +1,41 @@
+import Square from './geometrical-figures/square'
+import Projectile from './Projectile'
+
+class Player extends Square {
+    constructor(color, canvasContext, canvas) {
+        super(200, 200, 30, color, canvasContext, canvas);
+    }
+
+    move(position) {
+        switch (position) {
+            case 'a':
+            case 'A':
+            case "ArrowLeft":
+                super.moveLeft()
+                return
+            case "d":
+            case "D":
+            case 'right':
+                super.moveRight();
+                return
+            case "w":
+            case "W": 
+            case "ArrowTop":
+                super.moveTop();
+                return 
+            case "s":
+            case "S":
+            case 'ArrowDown':
+                super.moveDown();
+                return
+            default:
+                return;
+        }
+    }
+
+    fight(projectileTarget) {
+        
+    }
+}
+
+export default Player
