45fbbd1c1317325b0de672228eaa5d8e3a167783
Hugo Meireles <hugo.meireles@edirectinsurance.com>
2021-01-17 03:04:49 +0000

    create request animation frame

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
+import { useEffect, useRef } from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+import Game from '../game/Game'
 
 export default function Home() {
+  const canvasRef = useRef()
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const canvasContext = canvas.getContext('2d')
+    
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+    
+    const game = new Game(canvasContext, canvas)
+    
+    function render() {
+      requestAnimationFrame(render)
+      game.start()
+    }
+    render()
+
+  }, [])
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Fight Space</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      
+      <canvas 
+        id="game-screen"
+        ref={canvasRef}
+        
+      />
+    </>
   )
 }
