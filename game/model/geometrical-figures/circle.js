269216f63fd53c5289bb7b18d6ebbc2f4b01d6ad
Hugo Meireles <hugo.meireles@edirectinsurance.com>
2021-01-17 02:43:39 +0000

    update the clean before drow

--- game/model/geometrical-figures/circle.js
+++ game/model/geometrical-figures/circle.js
@@ -1,13 +1,15 @@
 class Circle {
-    constructor(x, y, radius, color, canvasContext) {
+    constructor(x, y, radius, color, canvasContext, canvas) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
         this.ctx = canvasContext
+        this.canvas = canvas;
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         this.ctx.beginPath();
         this.ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
         this.ctx.fillStyle = this.color;
--- game/model/geometrical-figures/square.js
+++ game/model/geometrical-figures/square.js
@@ -1,17 +1,43 @@
 class Square {
-    constructor(x, y, width, color, canvasContext) {
+    constructor(x, y, width, color, canvasContext, canvas) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.color = color;
         this.ctx = canvasContext
+        this.canvas = canvas;
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
         this.ctx.beginPath();
         this.ctx.rect(this.x, this.y, this.width, this.width);
         this.ctx.fillStyle = this.color;
         this.ctx.fill();
+    }
+
+    moveLeft() {
+        this.update()
+        this.x = this.x - 2
+    }
+
+    moveRight() {
+        this.update()
+        this.x = this.x + 2
+    }
+
+    moveTop() {
+        this.update()
+        this.y = this.y - 2
+    }
+
+    moveDown() {
+        this.update()
+        this.y = this.y + 2
+    }
+
+    update() {
+        this.draw()
     }
 }
 
