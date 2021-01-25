ed7e189421096b78588b01527f5808bb5d40d201
Hugo Meireles <meireles.hmm@gmail.com>
2021-01-25 23:47:14 +0000

    refactoring game state

--- game/game-state.js
+++ game/game-state.js
@@ -1,39 +1,26 @@
-import { DEFAULT_INITIAL_POSITION } from './configs'
-
-export const gameState = {
+const gameState = {
     playerState: {
-        name: "Player Ship",
         position: {
-            x: DEFAULT_INITIAL_POSITION.x,
-            y: DEFAULT_INITIAL_POSITION.y,
-            mouseClick: false
+            x: 100,
+            y: 100,
         },
         ship: {
-            color: 'white',
-            type: 'default',
-            width: 2,
-            radarRange: 1,
             health: 100,
-            armor: 0,
-            damage: 20
+            shield: 0,
         }
     },
     gameObjectState: [
         {
-            name: "Enemy 1",
             position: {
                 x: 300,
                 y: 400,
             },
             ship: {
-                color: 'red',
-                type: 'default',
-                width: 1.2,
-                radarRange: 0.8,
                 health: 100,
-                armor: 0,
-                damage: 20
+                shield: 0,
             }
         }
     ]
 }
+
+export default gameState
